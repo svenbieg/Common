26e6f7df4b897f2ba9ec3dc26cbf3452809cfd4a
Sven Bieg <svenbieg@web.de>
2024-06-06 22:00:00 +0200

    Initial commit

---
+++ Platform.h
@@ -0,0 +1,28 @@
+//============
+// Platform.h
+//============
+
+#pragma once
+
+
+//=======
+// Using
+//=======
+
+#include "Default/Platform.h"
+
+
+//========
+// Common
+//========
+
+#include "BoolClass.h"
+#include "CharClass.h"
+#include "Enum.h"
+#include "Flags.h"
+#include "FloatClass.h"
+#include "Function.h"
+#include "Guid.h"
+#include "IntegerClasses.h"
+#include "Sentence.h"
+#include "StringVariable.h"
