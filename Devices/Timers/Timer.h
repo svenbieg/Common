bb866841e482c5f7c91c999eaabee5114e53688f
Sven Bieg <svenbieg@web.de>
2024-11-03 09:53:30 +0100

    Update

---
+++ Devices/Timers/Timer.h
@@ -0,0 +1,43 @@
+//=========
+// Timer.h
+//=========
+
+#pragma once
+
+
+//===========
+// Namespace
+//===========
+
+namespace Devices {
+	namespace Timers {
+
+
+//=======
+// Timer
+//=======
+
+class Timer: public Object
+{
+public:
+	// Con-/Destructors
+	Timer();
+	~Timer();
+
+	// Common
+	BOOL IsStarted()const { return m_Interval!=0; }
+	VOID Reset();
+	VOID StartOnce(UINT MilliSeconds);
+	VOID StartPeriodic(UINT MilliSeconds);
+	VOID Stop();
+	Event<Timer> Triggered;
+
+private:
+	// Common
+	VOID DoTrigger();
+	VOID OnClockTick();
+	INT m_Interval;
+	UINT64 m_NextTime;
+};
+
+}}
